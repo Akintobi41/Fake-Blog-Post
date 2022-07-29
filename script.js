dabfe215a11aa98d9ec426f6c251438a4b7af834
Akintobi41 <akintobi41moyinoluwa@gmail.com>
2022-07-29 18:05:24 +0100

    started working on blog homepage and updated blog styles

---
+++ script.js
@@ -0,0 +1,43 @@
+let user = fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response)
+
+let section = document.querySelector('.section');
+
+section.classList.add('section');
+
+const loadCards = async () => {
+    let newNew = await user;
+  
+    // if (newNew.status === 200) {
+    
+    //   loadOut.style.display = "none";
+    //   loadOut.style.top = "initial";
+    //   loadOut.style.left = "initial";
+    // }
+  
+    let card = await newNew.json();
+  
+    card.forEach(function (item, index) {
+    
+      let div = document.createElement('div');
+    
+      let p = document.createElement('p');
+    
+      p.textContent = item.body;
+  
+      let img = document.createElement('img');
+      img.src = "/invalid.png";
+      img.classList.add('close-image');
+  
+      div.appendChild(p);
+      div.appendChild(img)
+      section.appendChild(div);
+      div.classList.add('mini-container');
+      p.classList.add('text-content');
+      p.id = index + 1;
+      img.addEventListener('click', deleteElement);
+    
+    });
+  
+}  
+     
