b595903a28f8d5da6f1e7e0cc609a1ef57e46754
Akintobi41 <akintobi41moyinoluwa@gmail.com>
2022-08-12 23:12:07 +0100

    updated Posts for editing and deleting

--- script.js
+++ script.js
@@ -93,3 +93,232 @@ const loadCards = async () => {
 
 loadCards();
 //
+
+// Submitting new content after posting
+let closeModal = async () => {
+  // Updated styles for submit button
+  close.style.backgroundColor = "transparent";
+  close.style.color = "#f5f5f5";
+  close.style.cursor = "not-allowed";
+
+  let user = fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: postTitle.value,
+      body: postText.value,
+      userId: newId,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+
+  }).then(data => data.json())
+
+  let newData = await user;
+
+  if (newData.status === 201) {
+    modal_container.style.display = "none";
+    modal_container.classList.remove('show');
+
+    section.style.display = "flex";
+    postFooter.style.display = "flex";
+
+    // Revert styles for Submit button
+    close.style.backgroundColor = "#3f4b9d";
+    close.style.cursor = "pointer";
+
+    let addedPost_data = await user,
+      usable_data = await addedPost_data.json(),
+
+      newUser = []; // Array to store new data 
+
+    newUser.push(usable_data)
+
+    newUser.forEach(function (elements) {
+      let firstContainer = document.querySelector('.mini-container'),
+        div = document.createElement('div'),
+        p = document.createElement('p');
+
+      p.textContent = elements.body;
+
+      let img = document.createElement('img'),
+        img2 = document.createElement('img');
+
+      img.src = "/invalid.png";
+      img2.src = "/create-outline.svg";
+
+      img.classList.add('close-image');
+      img2.classList.add('edit-content');
+
+      div.appendChild(p);
+      div.appendChild(img);
+      div.appendChild(img2);
+
+      section.insertBefore(div, firstContainer);
+
+      miniContainer = Array.from(miniContainer)  //  Updated miniContainer
+      miniContainer.unshift(div)                 // Updated search results to include newly added posts
+
+      div.classList.add('mini-container');
+      p.classList.add('text-content');
+
+      p.id = newId;
+
+      img.addEventListener('click', deleted, true);
+
+      img2.addEventListener('click', function (e) {
+
+        let postTitle = document.querySelector('.post-title'),
+
+          postText = document.querySelector('.post-text');
+
+        close.removeEventListener('click', closeModal);
+
+        postFooter.style.display = "none";
+        section.style.display = "none";
+        postTitle.value = e.target.previousSibling.previousSibling.id.title;
+        postText.value = e.target.previousSibling.previousSibling.textContent;
+
+        new_text = e.target.previousSibling.previousSibling;
+        if (postTitle.value == "undefined") {
+          postTitle.value = "No Title";
+        }
+
+        modal_container.style.display = "flex"
+        modal_container.classList.add('show');
+
+        let patchResource = async (e) => {
+
+          new_text.textContent = postText.value;
+
+          fetch('https://jsonplaceholder.typicode.com/posts/1', {
+            method: 'PATCH',
+            body: JSON.stringify({
+              title: postTitle.value,
+              body: postText.value
+
+            }),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          })
+            .then((response) => response.json())
+
+          modal_container.style.display = "none";
+          modal_container.classList.remove('show');
+          section.style.display = "flex";
+          postFooter.style.display = "flex";
+
+          e.stopImmediatePropagation();
+        }
+        close.addEventListener('click', patchResource);
+      })
+
+    });
+
+  }
+}
+
+
+// Posting new content
+open.addEventListener('click', postContent)
+
+function postContent(e) {
+  postTitle.value = '';                     // Empty input field
+  postText.value = '';                      // Empty input field
+
+  modal_container.style.display = "flex";   // Show Modal
+  modal_container.classList.add('show');    // Add class to the modal
+
+  section.style.display = "none";           // Hide content
+  postFooter.style.display = "none";        // Hide footer showing pagination
+
+  newId++;
+  close.addEventListener('click', closeModal, { once: true })   // Making posts post only once
+}
+
+const refreshBtn = document.querySelector('.all-posts');
+
+refreshBtn.addEventListener('click', loadCards)  // Refresh Page
+
+
+// Delete Items
+let deleted = async (e) => {
+
+  const loaderWrapper = document.querySelector(".loader-icon")
+  loaderWrapper.style.display = "grid"
+
+  try {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${e.target.previousSibling.id}`, {
+      method: 'DELETE',
+    })
+
+    if (res.ok == true) {
+      let child = e.target.parentElement;
+      child.parentElement.removeChild(child)
+    }
+  }
+  catch (e) {
+    console.error(e)
+  }
+  finally {
+    e.stopPropagation();
+    loaderWrapper.style.display = "none";
+  }
+
+};
+//
+
+
+// Editing Content
+
+let runEdit = async (e) => {
+  e.stopPropagation();   // new change
+  modal_container.style.display = "flex";
+
+  try {
+    let new_card = await fetch('https://jsonplaceholder.typicode.com/posts'),
+      card1 = await new_card.json()
+
+    postFooter.style.display = "none";
+
+    section.style.display = "none";
+
+    postTitle.value = card1[e.target.previousSibling.previousSibling.id].title;
+
+    postText.value = e.target.previousSibling.previousSibling.textContent;
+
+    new_text = e.target.previousSibling.previousSibling;
+
+    modal_container.classList.add('show');
+  }
+  catch (error) {
+    console.error("Error occurred during editing")
+  }
+
+  let patchResource = async () => {
+    new_text.textContent = postText.value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts/`${card[e.target.previousSibling.previousSibling.id].id}`', {
+
+      method: 'PATCH',
+      body: JSON.stringify({
+        title: postTitle.value,
+        body: postText.value
+
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+
+    modal_container.style.display = "none";
+    modal_container.classList.remove('show');
+    section.style.display = "flex";
+    postFooter.style.display = "flex";
+
+  }
+  close.addEventListener('click', patchResource, { once: true });
+};
+// Editing content 
